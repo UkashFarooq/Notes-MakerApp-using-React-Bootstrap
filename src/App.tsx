3d636392306f28e7b9fa1e9bfb1ccc938c975a8b
Ukash Farooq <ukashfarooq10@gmail.com>
2022-03-09 17:46:22 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import React,{useState} from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './App.css';
+import CreateNotes from './components/CreateNotes';
+import Header from './components/header';
+import NotesList from './components/NotesList';
+import { Note } from './models/note.model';
+
+
+
+
+function App() {
+  const [notes, setNotes] = useState<Note[]>([{
+    id: (new Date).toString(),
+    title: "Meetings",
+    text: "Schedule meeting with team.",
+    color:"#dfdfdf",
+    date:(new Date).toString()
+  }]);
+
+
+
+
+  return (
+    <>
+    < Header/>
+    <Container className='mt-5'>
+      <Row>
+        <Col>
+        < NotesList notes ={notes} setNotes ={setNotes}/>
+
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        < CreateNotes notes={notes} setNotes ={setNotes}/>
+
+        </Col>
+      </Row>
+    </Container>
+    
+    
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Container, Navbar } from 'react-bootstrap';
+
+interface IHeaderProps {
+}
+
+const Header: React.FunctionComponent<IHeaderProps> = (props) => {
+  return(
+      <Navbar fixed="top" bg="dark" variant ="dark">
+          <Container>
+              <Navbar.Brand>
+                  Hold my notes..
+              </Navbar.Brand>
+
+
+
+
+          </Container>
+          
+          
+          
+          </Navbar>
+  ) ;
+};
+
+export default Header;
